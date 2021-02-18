7b99e378a9700ec33413173a96af05fa1bc62585
BoaDelay <78747561+BoaDelay@users.noreply.github.com>
2021-02-18 20:12:35 +0300

    Stack on defined dynamic memory  - without frill
    Every standart stack function works normally

---
+++ StackOnDynamicMemory/StackOnDynamicMemory.cpp
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <cstdlib>
+
+struct Stack
+{
+	int* memory_pointer;
+	unsigned int size;
+	unsigned int amount_of_elements;
+};
+
+void Stack_Init(Stack* x)
+{
+	x->size = 10;
+	x->amount_of_elements = 0;
+	x->memory_pointer = (int*)malloc(x->size * sizeof(int));
+}
+
+void Stack_Delete(Stack* x)
+{
+	free(x->memory_pointer);
+}
+
+bool IsFull(Stack* x)
+{
+	if (x->amount_of_elements == 10)
+	{
+		printf("Stack is full\n");
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool IsEmpty(Stack* x)
+{
+	if (x->amount_of_elements == 0)
+	{
+		printf("Stack is empty\n");
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void Push(Stack* x, int stack_data)
+{
+	if (IsFull(x))
+	{
+		printf("Can not Push\n");
+	}
+	else
+	{
+		x->memory_pointer[x->amount_of_elements++] = stack_data;
+		printf("%d was added to the stack\n", x->memory_pointer[x->amount_of_elements - 1]);
+	}
+}
+
+void Pop(Stack* x)
+{
+	if (IsEmpty(x))
+	{
+		printf("Can not Pop\n");
+	}
+	else
+	{
+		printf("%d was deleted from the stack\n", x->memory_pointer[x->amount_of_elements - 1]);
+		x->memory_pointer[--x->amount_of_elements] = NULL;
+	}
+}
+
+void ShowAll(Stack* x)
+{
+	for (int i = 0; i < x->amount_of_elements; i++)
+	{
+		printf("%d\n", x->memory_pointer[i]);
+	}
+}
+
+void Top(Stack* x)
+{
+	printf("The last element is %d\n", x->memory_pointer[x->amount_of_elements - 1]);
+}
+
+void Size(Stack* x)
+{
+	printf("There are %d elements in the stack\n", x->amount_of_elements);
+}
+
+int main()
+{
+	Stack x;
+
+	Stack_Init(&x);
+
+	Push(&x, 4);
+
+	Size(&x);
+
+	Top(&x);
+
+	Pop(&x);
+
+	Pop(&x);
+
+	Push(&x, 1);
+	Push(&x, 3);
+	Push(&x, 2);
+	Push(&x, 5);
+	Push(&x, 4);
+	Push(&x, 7);
+	Push(&x, 6);
+	Push(&x, 9);
+	Push(&x, 8);
+	Push(&x, 0);
+	ShowAll(&x);
+
+	Push(&x, 1);
+
+	Stack_Delete(&x);
+	return 0;
+}
