3f3297c602e5ea17726e6536f5411883aef6a3b2
BoaDelay <78747561+BoaDelay@users.noreply.github.com>
2021-03-29 17:01:51 +0300

    Update 2List.cpp

--- 2List/2List.cpp
+++ 2List/2List.cpp
@@ -51,6 +51,8 @@ node* AddBegin(doublelinkedlist* x, int data)
 	temp->prev = x->begin;
 	temp->data = data;
 
+	x->size++;
+
 	printf("Element was added to the beginning of the list\n");
 
 	return temp;
@@ -63,6 +65,8 @@ node* AddEnd(doublelinkedlist* x, int data)
 	x->end->prev = temp;
 	temp->next = x->end;
 	temp->data = data;
+
+	x->size++;
 
 	printf("Element was added to the end of the list\n");
 
@@ -85,6 +89,8 @@ void DeleteBegin(doublelinkedlist* x)
 	x->begin->next = slider;
 	slider->prev = x->begin;
 
+	x->size--;
+
 	printf("Element was deleted from the beginning of the list\n");
 }
 
@@ -104,7 +110,105 @@ void DeleteEnd(doublelinkedlist* x)
 	x->end->prev = slider;
 	slider->next = x->end;
 
+	x->size--;
+
 	printf("Element was deleted from the end of the list\n");
+}
+
+void ShowAll(const doublelinkedlist* x)
+{
+	node* slider = x->begin;
+
+	while (slider != NULL)
+	{
+		printf("%d\n", slider);
+
+		slider = slider->next;
+	}
+}
+
+node* AddFree(doublelinkedlist* x, int index, int data)
+{
+	node* temp = (node*)malloc(sizeof(node));
+
+	if (index <= x->size / 2)
+	{
+		node* slider = x->begin;
+		
+		for (int i = 1; i != index - 1; i++)
+		{
+			slider = slider->next;
+		}
+
+		node* temp_slider = slider->next;
+
+		temp->next = temp_slider;
+		temp->prev = slider;
+		temp->data = data;
+	}
+	else
+	{
+		node* slider = x->end;
+
+		for (int i = x->size; i != index + 1; i--)
+		{
+			slider = slider->prev;
+		}
+
+		node* temp_slider = slider->prev;
+
+		temp->next = slider;
+		temp->prev = temp_slider;
+		temp->data = data;
+	}
+
+	x->size++;
+
+	printf("Element was added to the %d posision of the list\n", index);
+
+	return temp;
+}
+
+void DeleteFree(doublelinkedlist* x, int index)
+{
+	if (index <= x->size / 2)
+	{
+		node* slider = x->begin;
+
+		for (int i = 1; i != index - 1; i++)
+		{
+			slider = slider->next;
+		}
+
+		node* temp_slider = slider->next;
+		temp_slider = temp_slider->next;
+
+		free(slider->next);
+
+		slider->next = temp_slider;
+		temp_slider->prev = slider;
+	}
+	else
+	{
+		node* slider = x->end;
+
+		for (int i = x->size; i != index + 1; i--)
+		{
+			slider = slider->prev;
+		}
+
+		node* temp_slider = slider->prev;
+		temp_slider = temp_slider->prev;
+
+		free(slider->prev);
+
+		slider->prev = temp_slider;
+		temp_slider->next = slider;
+	}
+
+	x->size--;
+
+	printf("Element was deleted from the %d posision of the list\n", index);
 }
 
 int main()
